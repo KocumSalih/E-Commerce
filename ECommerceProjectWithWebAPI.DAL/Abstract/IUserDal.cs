dc39bef07cc5a88865cd7bb98d4fde7c8d6613c7
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 19:59:55 +0300

    IUserDal and  EfUserDal created in DAL

---
+++ ECommerceProjectWithWebAPI.DAL/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.DAL.Abstract
+{
+    using Entities.Concrete;
+
+    public interface IUserDal:IBaseRepository<User>
+    {
+    }
+}
---
+++ ECommerceProjectWithWebAPI.DAL/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceProjectWithWebAPI.DAL.Concrete.EntityFramework
+{
+    using DAL.Abstract;
+    using DAL.Concrete.Contexts;
+    using Entities.Concrete;
+
+    public  class EfUserDal : EFBaseRepository<User,ECommerceProjectWithWebAPIContext>,IUserDal
+    {
+
+    }
+}
