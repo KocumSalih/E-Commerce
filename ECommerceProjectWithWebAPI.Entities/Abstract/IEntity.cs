84c2e9021fa29efde31eb314ac65ed737a24b197
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 04:15:09 +0300

    IEntity created in Entities-Abstract

---
+++ ECommerceProjectWithWebAPI.Entities/Abstract/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Entities.Abstract
+{
+    /// <summary>
+    /// Veritabanına karşılık gelen tablolara implemente edilecek.
+    /// </summary>
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
