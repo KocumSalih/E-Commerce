771523235311247647e3de297f9dff1cc512eef3
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 21:51:19 +0300

    Added dependency and DbContext in ConfigureServices

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,15 +1,13 @@
+using ECommerceProjectWithWebAPI.DAL.Abstract;
+using ECommerceProjectWithWebAPI.DAL.Concrete.Contexts;
+using ECommerceProjectWithWebAPI.DAL.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebAPI
 {
@@ -25,8 +23,9 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ECommerceProjectWithWebAPIContext>(options => options.UseSqlServer("Server=DESKTOP-DPKMFQT\\S2019; Database=ECommerceProjectWithWebAPI; uid=sa; pwd=1;"));
             services.AddControllers();
+            services.AddTransient<IUserDal, EfUserDal>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
