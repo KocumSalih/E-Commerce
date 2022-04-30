764d77c8747b17da41ef662b246c82694ab1a656
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 04:16:04 +0300

    AuditTableEntity created in Entities-Concrete-BaseEntity

---
+++ ECommerceProjectWithWebAPI.Entities/Concrete/BaseEntities/AuditTableEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Entities.Concrete.BaseEntities
+{
+    using Abstract;
+
+    public class AuditTableEntity : BaseEntity, ICreatedEntity,IUpdatedEntity
+    {
+        public int CreatedUser { get ; set ; }
+        public DateTime CreatedDate { get; set; }
+        int? IUpdatedEntity.CreatedUser { get; set; }
+        DateTime? IUpdatedEntity.CreatedDate { get; set ; }
+    }
+}
