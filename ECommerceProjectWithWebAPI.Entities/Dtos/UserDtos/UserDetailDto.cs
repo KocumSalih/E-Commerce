fbb924a4a19231b615c0c8badea60d6045e2b012
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 21:49:19 +0300

    Added Id property inside UserDetailDto

--- ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/UserDetailDto.cs
+++ ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -10,10 +10,10 @@ namespace ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos
 
     public class UserDetailDto:IDto
     {
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Password { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
