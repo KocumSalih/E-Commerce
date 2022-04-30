c8b5ab9bd871bc82298fc4dca520dcb5852a4400
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 04:15:23 +0300

    IUpdatedEntity created in Entities-Abstract

---
+++ ECommerceProjectWithWebAPI.Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int? CreatedUser { get; set; }
+        DateTime? CreatedDate { get; set; }
+    }
+}
