b797c6a96df9265258e48190bc2fed0817b495f9
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 20:01:14 +0300

    ECommerceProjectWithWebAPIContext created in DAL

---
+++ ECommerceProjectWithWebAPI.DAL/Concrete/Contexts/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceProjectWithWebAPI.DAL.Concrete.Contexts
+{
+    using DAL.Concrete.EntityFramework.Mapping;
+    using Entities.Concrete;
+
+    public class ECommerceProjectWithWebAPIContext : DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext> options) : base(options)
+        {
+        }
+
+        public ECommerceProjectWithWebAPIContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=DESKTOP-DPKMFQT\\S2019; Database=ECommerceProjectWithWebAPI; uid=sa; pwd=1;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public virtual DbSet<User> Users {get; set;}
+    }
+}
