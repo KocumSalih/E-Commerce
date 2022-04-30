15541e4726ff08131523db14b5cc2c337f319a44
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 21:46:09 +0300

    IUserService created in Entities.

---
+++ ECommerceProjectWithWebAPI.Business/Abstract/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Business.Abstract
+{
+    using ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos;
+
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+        Task<UserDto> GetByIdAsync(int id);
+        Task<UserDto> AddAsync(UserAddDto entity);
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
