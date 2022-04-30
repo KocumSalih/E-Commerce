fbfadba6498d335fd1e68667cb806ee39a48d6ea
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-04-30 04:14:16 +0300

    ICreatedEntity created in Entities-Abstract

---
+++ ECommerceProjectWithWebAPI.Entities/Abstract/ICreatedEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Entities.Abstract
+{
+    public interface ICreatedEntity
+    {
+        int CreatedUser { get; set; }
+        DateTime CreatedDate { get; set; }
+
+    }
+}
