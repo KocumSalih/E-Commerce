dae8b2bd42d7e55e79ae7663e6a6dfff0014407d
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-02 01:58:50 +0300

    Revert "Users were withdrawn via api when the form was loaded"
    This reverts commit 417cf62c341f6c91e7f5e2f258ff8cc3ab8f41be.

--- WebAPIWithWindowsForm/Form1.cs
+++ WebAPIWithWindowsForm/Form1.cs
@@ -1,27 +1,20 @@
-﻿using ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WebAPIWithWindowsForm
 {
     public partial class Form1 : Form
     {
-        private readonly string url = "http://localhost:62853/api/Users/GetList";
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            using (HttpClient httpClient=new HttpClient())
-            {
-                var users = await httpClient.GetFromJsonAsync<List<UserDetailDto>>(new Uri(url));
-                dataGridView1.DataSource = users;
-            }
         }
     }
 }
