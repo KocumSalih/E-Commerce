ea4df737cbe38dd069b11c09828b9fd47b0e620e
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-05 20:43:23 +0300

    Password property has been added to UserDto class.

--- ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/UserDto.cs
+++ ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/UserDto.cs
@@ -10,6 +10,7 @@ namespace ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public bool Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
