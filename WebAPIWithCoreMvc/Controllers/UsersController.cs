cb57e0e78a5aec1f118d110cff7922cbd0210517
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-05 18:24:40 +0300

    Add action method added to UserController

--- WebAPIWithCoreMvc/Controllers/UsersController.cs
+++ WebAPIWithCoreMvc/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using WebAPIWithCoreMvc.ViewModels;
 
 namespace WebAPIWithCoreMvc.Controllers
 {
@@ -28,6 +29,48 @@ namespace WebAPIWithCoreMvc.Controllers
         {
             var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url+"Users/GetList");
             return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            ViewBag.GenderList = GenderFill();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(UserAddViewModel userAddViewModel)
+        {
+            UserAddDto userAddDto = new UserAddDto()
+            {
+                FirstName = userAddViewModel.FirstName,
+                LastName = userAddViewModel.LastName,
+                Gender = userAddViewModel.GenderId == 1 ? true : false,
+                Address=userAddViewModel.Address,
+                DateOfBirth=userAddViewModel.DateOfBirth,
+                Email=userAddViewModel.Email,
+                Password=userAddViewModel.Password,
+                UserName=userAddViewModel.UserName,
+            };
+            HttpResponseMessage responseMessage = await _httpClient.PostAsJsonAsync(url+"Users/Add",userAddDto);
+            if (responseMessage.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            return View();
+        }
+
+        private List<Gender> GenderFill()
+        {
+            List<Gender> genders = new List<Gender>();
+            genders.Add(new Gender(){Id=1,GenderName="Erkek"});
+            genders.Add(new Gender(){Id=2,GenderName="Kadın"});
+            return genders;
+        }
+
+        private class Gender
+        {
+            public int Id { get; set; }
+            public string GenderName { get; set; }
         }
     }
 }
