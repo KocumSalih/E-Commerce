4d00922a16dfea904b87093737f2056b0ef96a0b
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 02:49:13 +0300

    ITokenService class created.

---
+++ ECommerceProjectWithWebAPI.Core/Utilities/Security/Token/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceProjectWithWebAPI.Core.Helpers.JWT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceProjectWithWebAPI.Core.Utilities.Security.Token
+{
+    public interface ITokenService
+    {
+        AccessToken CreateToken(int userId, string userName);
+    }
+}
