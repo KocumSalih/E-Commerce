0b1e3e92b78e0d7cb56ace0fc5bd22c481cf367d
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 02:24:37 +0300

    The UserForLoginDto class has been created.

---
+++ ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos
+{
+    public class UserForLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
