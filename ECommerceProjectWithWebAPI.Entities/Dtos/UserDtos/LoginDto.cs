254b751a2f36131157dca0e8accce360e33ef5dd
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 12:20:42 +0300

    IsRememberMe property added in LoginDto

---
+++ ECommerceProjectWithWebAPI.Entities/Dtos/UserDtos/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos
+{
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsRememberMe { get; set; }
+    }
+}
