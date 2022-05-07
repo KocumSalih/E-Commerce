d86eddf142a26b4c3443c8cddfea9ffc164a53bc
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 12:16:04 +0300

    IAuthService created.

---
+++ ECommerceProjectWithWebAPI.Business/Abstract/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceProjectWithWebAPI.Core.Utilities.Responses;
+using ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos;
+using System.Threading.Tasks;
+
+namespace ECommerceProjectWithWebAPI.Business.Abstract
+{
+    public interface IAuthService
+    {
+        Task<ApiDataResponse<UserDto>> LoginAsync(LoginDto loginDto);
+    }
+}
