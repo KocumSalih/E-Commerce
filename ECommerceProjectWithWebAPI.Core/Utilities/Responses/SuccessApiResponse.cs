0060cb37f761897d6b16c839817583cb44166583
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 03:43:11 +0300

    Added the Responses folder to the Utilities folder in the Core layer. SuccessApiResponse  class created.

---
+++ ECommerceProjectWithWebAPI.Core/Utilities/Responses/SuccessApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace ECommerceProjectWithWebAPI.Core.Utilities.Responses
+{
+    public class SuccessApiResponse:ApiResponse
+    {
+        public SuccessApiResponse() : base(success: true)
+        {
+
+        }
+
+        public SuccessApiResponse(string message):base(success:true,message:message)
+        {
+
+        }
+    }
+}
