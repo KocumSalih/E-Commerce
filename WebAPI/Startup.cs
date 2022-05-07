480dd24e73a1b8fa3d4cc4e7bcf9a732ea65528a
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 11:08:03 +0300

    Added ConfigureServices AddProfile service in Startup.cs in WebAPI project.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using ECommerceProjectWithWebAPI.Business.Abstract;
 using ECommerceProjectWithWebAPI.Business.Concrete;
+using ECommerceProjectWithWebAPI.Business.Mappings;
 using ECommerceProjectWithWebAPI.Core.Helpers.JWT;
 using ECommerceProjectWithWebAPI.Core.Utilities.Security.Token;
 using ECommerceProjectWithWebAPI.Core.Utilities.Security.Token.Jwt;
@@ -32,20 +34,20 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ECommerceProjectWithWebAPIContext>(options => options.UseSqlServer("Server=DESKTOP-DPKMFQT\\S2019; Database=ECommerceProjectWithWebAPI; uid=sa; pwd=1;"));
-            
-            services.AddControllers();            
+
+            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
 
-                c.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme() 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
-                    Name="Authorization",
-                    Type=SecuritySchemeType.ApiKey,
-                    Scheme="Bearer",
-                    BearerFormat="JWT",
-                    In=ParameterLocation.Header,
-                    Description="Bearer þemasýný kullanan JWT yetkilendirme baþlýðý \r\n\r\n 'Bearer' [Boþluk] giriniz."
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Bearer þemasýný kullanan JWT yetkilendirme baþlýðý \r\n\r\n 'Bearer' [Boþluk] giriniz."
                 });
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement()
@@ -64,16 +66,18 @@ namespace WebAPI
                 });
             });
 
+            #region JWT
+
             var appSettingsSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
 
             var appSettings = appSettingsSection.Get<AppSettings>();
             var key = Encoding.ASCII.GetBytes(appSettings.SecurityKey);
-            services.AddAuthentication(x=>
+            services.AddAuthentication(x =>
             {
                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x=>
+            }).AddJwtBearer(x =>
             {
                 x.RequireHttpsMetadata = false;
                 x.SaveToken = true;
@@ -85,11 +89,24 @@ namespace WebAPI
                     ValidateAudience = false
                 };
             });
+            #endregion
 
+            #region AutoMapper
 
+            var mapperConfig = new MapperConfiguration(mc =>
+              {
+                  mc.AddProfile(new MappingProfile());
+              });
+            var mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            #endregion
+
+            #region DI
             services.AddTransient<IUserDal, EfUserDal>();
             services.AddTransient<IUserService, UserService>();
-            services.AddTransient<ITokenService, JwtTokenService>();
+            services.AddTransient<ITokenService, JwtTokenService>(); 
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
