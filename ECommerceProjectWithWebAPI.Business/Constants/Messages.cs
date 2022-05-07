381ab134a519d4d46a6ae874a2c969c851dff44b
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 12:20:02 +0300

    Messages.SystemLoginFailed added.

--- ECommerceProjectWithWebAPI.Business/Constants/Messages.cs
+++ ECommerceProjectWithWebAPI.Business/Constants/Messages.cs
@@ -13,5 +13,8 @@ namespace ECommerceProjectWithWebAPI.Business.Constants
         public const string Updated = "Güncellendi";
         public const string Listed = "Listelendi";
         public const string NotListed = "Listelenmedi";
+        public const string UserNotFound = "Kullanıcı bulunamadı";
+        public const string SystemLoginSuccessful = "Sisteme giriş başarılı";
+        public const string SystemLoginFailed = "Sisteme giriş başarısız";
     }
 }
