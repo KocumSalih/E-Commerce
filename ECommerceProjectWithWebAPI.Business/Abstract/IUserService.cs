b6bedf76fbea42f845ffd74d6a27cae0bb4c96df
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-07 11:06:32 +0300

    Added Expression to IUserService GetListAsync and GetAsync methods.

--- ECommerceProjectWithWebAPI.Business/Abstract/IUserService.cs
+++ ECommerceProjectWithWebAPI.Business/Abstract/IUserService.cs
@@ -1,14 +1,17 @@
-﻿using ECommerceProjectWithWebAPI.Core.Helpers.JWT;
-using ECommerceProjectWithWebAPI.Core.Utilities.Responses;
+﻿using ECommerceProjectWithWebAPI.Core.Utilities.Responses;
+using ECommerceProjectWithWebAPI.Entities.Concrete;
 using ECommerceProjectWithWebAPI.Entities.Dtos.UserDtos;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace ECommerceProjectWithWebAPI.Business.Abstract
 {
     public interface IUserService
     {
-        Task<ApiDataResponse<IEnumerable<UserDetailDto>>> GetListAsync();
+        Task<ApiDataResponse<IEnumerable<UserDetailDto>>> GetListAsync(Expression<Func<User, bool>> filter = null);
+        Task<ApiDataResponse<UserDto>> GetAsync(Expression<Func<User, bool>> filter);
         Task<ApiDataResponse<UserDto>> GetByIdAsync(int id);
         Task<ApiDataResponse<UserDto>> AddAsync(UserAddDto entity);
         Task<ApiDataResponse<UserUpdateDto>> UpdateAsync(UserUpdateDto entity);
