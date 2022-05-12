0e42fec5af8f8d8c339734d6c3f436b3fbb31d73
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-12 13:30:00 +0300

    AuthApiService has been created.

---
+++ WebAPIWithCoreMvc/ApiServices/AuthApiService.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceProjectWithWebAPI.Core.Utilities.Responses;
+using ECommerceProjectWithWebAPI.Entities.Dtos.Auth;
+using ECommerceProjectWithWebAPI.Entities.Dtos.User;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
+
+namespace WebAPIWithCoreMvc.ApiServices
+{
+    public class AuthApiService : IAuthApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ApiDataResponse<UserDto>> LoginAsync(LoginDto loginDto)
+        {
+            HttpResponseMessage httpResponseMessage = await _httpClient.PostAsJsonAsync("Auth/Login", loginDto);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var data = await httpResponseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<ApiDataResponse<UserDto>>(data);
+                return await Task.FromResult(result);
+            }
+            return null;
+        }
+    }
+}
