efa8dbcadaf17c256f2b59dac0cd3ed455cb7e4b
KocumSalih <92206189+KocumSalih@users.noreply.github.com>
2022-05-12 13:30:17 +0300

    UserApiService has been created.

---
+++ WebAPIWithCoreMvc/ApiServices/UserApiService.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceProjectWithWebAPI.Core.Utilities.Responses;
+using ECommerceProjectWithWebAPI.Entities.Dtos.User;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
+
+namespace WebAPIWithCoreMvc.ApiServices
+{
+    public class UserApiService : IUserApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<UserDetailDto>> GetListAsync()
+        {
+            var response = await _httpClient.GetAsync("Users/GetList");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var responseSuccess = await response.Content.ReadFromJsonAsync<ApiDataResponse<IEnumerable<UserDetailDto>>>();
+            return responseSuccess.Data.ToList();
+        }
+    }
+}
